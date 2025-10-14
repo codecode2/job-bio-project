6de675ab0da2451d5c153b7dbba851f217259cb3
codecode2 <74464480+codecode2@users.noreply.github.com>
2025-10-14 17:41:22 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="name-title-container">
       <h1 className="name">Alcibiades Mirovalis</h1>
-      <h2 className="job-title">Software Engineer</h2>
+      <h2 className="job-title">Software Developer</h2>
     </div>
   )
 }
