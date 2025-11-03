44f2876abf3f4aaa1f9729071c53cef22ccab10c
codecode2 <74464480+codecode2@users.noreply.github.com>
2025-11-03 16:54:55 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/job-bio-project/',
+  base: '/',  // <-- set this to '/' for custom domain root
   plugins: [react()],
-})
+});
